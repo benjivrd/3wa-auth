a47b3ef3cac5773930eb2b9246df6f0c0f1bf936
benjamin verdier <benjamin@asapp.agency>
2023-04-21 14:59:34 +0200

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,27 @@ import {signup, login} from "../controllers/user.js";
 const router = express.Router();
 
 
-router.get('/signup', (_req, res) => {
+router.get('/signup', (req, res) => {
+  if(req.session.isConnected){
+    res.redirect('/');
+  }
   res.render('signup', { errors: null});
 });
 
-router.get('/login', (_req, res) => {
+router.get('/login', (req, res) => {
+  if(req.session.isConnected){
+    res.redirect('/');
+  }
   res.render('login', { errors: null});
 });
+
+router.get('/logout', (req ,res) => {
+  if(!req.session.isConnected){
+    res.redirect('/login');
+  }
+  req.session.destroy()
+  res.redirect('/');
+})
 
 router.post('/signup', signup);
 router.post('/login', login);
