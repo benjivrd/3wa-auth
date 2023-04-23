5d3f222bd6211df33ecec60bf38ca7d40fe8f050
benjamin verdier <benjamin@asapp.agency>
2023-04-23 07:22:11 +0200

    add reset password system

--- controllers/user.js
+++ controllers/user.js
@@ -9,7 +9,10 @@ export async function signup(req, res) {
   }
 
   if (password.length < 6) {
-    errors.push({ code: 2, msg: "Le mot de passe doit contenir au moins 6 caractères" });
+    errors.push({
+      code: 2,
+      msg: "Le mot de passe doit contenir au moins 6 caractères",
+    });
   }
 
   if (password !== repassword) {
@@ -22,10 +25,10 @@ export async function signup(req, res) {
     try {
       const userExists = await User.findOne({ email: email });
       if (userExists) {
-        errors.push({ code: 4 , msg: "Cet email est déjà utilisé" });
+        errors.push({ code: 4, msg: "Cet email est déjà utilisé" });
         res.render("signup", { errors, firstname, lastname, email, password });
       } else {
-        const salt = await bcrypt.genSalt(parseInt(process.env.SALT));
+        const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(password, salt);
         const newUser = new User({
           firstName: firstname,
@@ -34,7 +37,7 @@ export async function signup(req, res) {
           password: hash,
         });
         await newUser.save();
-        req.flash('success', 'Inscription réussis !')
+        req.flash("success", "Inscription réussis !");
         res.redirect("/login");
       }
     } catch (error) {
@@ -57,16 +60,83 @@ export async function login(req, res) {
     }
 
     if (errors.length > 0) {
-      res.render("login", { errors , email, password});
+      res.render("login", { errors, email, password });
     } else {
       req.session.isConnected = true;
       req.session.lastName = user.lastName;
       req.session.firstName = user.firstName;
       req.session.email = user.email;
-      req.flash('success', ' Authentification réussie !');
+      req.session.userId = user.id;
+      req.flash("success", " Authentification réussie !");
       res.redirect("/dashboard");
     }
   } catch (error) {
     res.status(500).send(error.message);
+  }
+}
+
+export async function resetPassword(req, res) {
+  const { password, newpassword, renewpassword } = req.body;
+  const { lastName, firstName, userId, email } = req.session;
+  const errors = [];
+
+  if (!password || !newpassword || !renewpassword) {
+    errors.push({ code: 0, msg: "Veuillez remplir tous les champs" });
+  }
+  if (password === newpassword) {
+    errors.push({
+      code: 2,
+      msg: "Le nouveau mot de passe ne peux pas être le même que l'ancien !",
+    });
+  }
+  if(newpassword.length < 6){
+    errors.push({
+      code: 4,
+      msg: "Le nouveau mot de passe doit contenir au moins 6 caractères",
+      });
+  }
+  if (renewpassword != newpassword) {
+    errors.push({
+      code: 3,
+      msg: "Les nouveau mots de passe ne sont pas identiques",
+    });
+  }
+  if (errors.length > 0) {
+    res.render("dashboard", {
+      errors,
+      lastName,
+      firstName,
+      email,
+      password,
+      newpassword,
+      renewpassword,
+    });
+  } else {
+    try {
+      const user = await User.findById(userId);
+      if (user) {
+        if (!bcrypt.compareSync(password, user.password)) {
+          errors.push({ code: 1, msg: "Le mot de passe est incorrect" });
+          res.render("dashboard", {
+            errors,
+            password,
+            newpassword,
+            renewpassword,
+            email,
+            firstName,
+            lastName,
+          });
+        } else {
+          const salt = await bcrypt.genSalt(10);
+          const hash = await bcrypt.hash(newpassword, salt);
+          user.password = hash;
+          user.save();
+          req.flash("success", "Mot de passe réinitialisé");
+          res.redirect("/dashboard");
+        }
+      }
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
 }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,15 +1,16 @@
 import express from "express";
-import { secureAuth } from "../middleware/secure.js"
+import { secureAuth } from "../middleware/secure.js";
+import { resetPassword } from "../controllers/user.js";
 
 const router = express.Router();
 
+router.get("/dashboard", secureAuth, (req, res) => {
+  const { firstName, lastName, email} = req.session;
+  res.render("dashboard", { firstName, lastName, email , errors: null});
+});
 
-router.get('/dashboard', secureAuth,  (req, res) => {
+router.post("/dashboard/reset-password", secureAuth, resetPassword);
 
-    const { firstName, lastName, email } = req.session;
-    res.render('dashboard',{ firstName, lastName, email});
-    
-  });
-  
-  
-export const DashboardRouter = router
+
+
+export const DashboardRouter = router;
